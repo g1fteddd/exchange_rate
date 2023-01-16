80daf36df89ad62813f55a3816ed77edd196f336
Safiullin Rufat <stud0000229588@study.utmn.ru>
2023-01-16 16:25:36 +0500

    удалил лишний компонент

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { paginate, LightPaginationNav } from "svelte-paginate";
-    import Currency from "./components/Currency.svelte";
 
 	interface ICurrency {
 		Changes: number;
